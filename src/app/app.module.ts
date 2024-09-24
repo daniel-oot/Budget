22b47c1ff19408764358b869b0d1811d5f40adc1
Daniel-oot <omoijiadedaniel0@gmail.com>
2024-09-24 16:25:27 -0500

    styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { MonthlyComponent } from './monthly/monthly.component';
 import { BiWeeklyComponent } from './bi-weekly/bi-weekly.component';
 import { WeeklyComponent } from './weekly/weekly.component';
+import { PrimeIcons } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { PrimeTemplate } from 'primeng/api';
+import { Button } from 'primeng/button';
+import {ToolbarModule} from 'primeng/toolbar'
+import {StepperModule} from 'primeng/stepper';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +26,18 @@ import { WeeklyComponent } from './weekly/weekly.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolbarModule,
+    StepperModule,
+    ButtonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ 
+    PrimeIcons,
+    PrimeTemplate,
+    PrimeNGConfig,
+    Button
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
